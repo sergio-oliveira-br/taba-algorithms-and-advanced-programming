33d14ce55d6d0a281c908af738b1154c038c4f46
Sergio Oliveira <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-05-15 09:42:47 +0100

    Add files via upload

---
+++ tabaaap/src/main/java/org/taba/question1/Reader.java
@@ -0,0 +1,21 @@
+/** National College of Ireland - NCI
+ * Higher Diploma in Science in Computing
+ *              ---
+ * Algorithms and Advanced Programming
+ * Lecturer: Dr. William Clifford
+ *              ---
+ * Student: Sergio Vinicio da Silva Oliveira
+ * x23170981@student.ncirl.ie
+ *              ---
+ * Question 1: Using a data file you have NOT previously worked on for the CA
+ * Chosen File: staff.csv
+ *              ---
+ * a)	Read a CSV file into memory using the Buffered reader
+ */
+
+package org.taba.question1;
+
+public class Reader
+{
+
+}
